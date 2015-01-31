2dcddb976c6d3d38f61ed3e101ac6a47e1207628
Michael Imfeld <michi.imfeld@gmail.com>
2015-01-31 16:24:33 +0100

    more options/args added, arg parsing updated

--- main.py
+++ main.py
@@ -7,26 +7,25 @@ import subprocess
 
 def main():
     parser = OptionParser()
-    parser.add_option("-t", "--target", dest="target", help="target ip in network to scan -t 192.168.192.0.10/24")
-    parser.add_option("-w", "--timeout", dest="timeout", help="ping timeout, default: 2")
+    parser.add_option("-t", "--target", dest="target", help="Target ip in network to scan -t 192.168.192.0.10/24")
+    parser.add_option("-w", "--timeout", dest="timeout", help="Ping timeout, default: 2")
+    parser.add_option("-c", "--count", dest="count", help="Stop after sending <count> requests to host, default: 1")
+    parser.add_option("-i", "--interval", dest="interval", help="Wait <interval> seconds between sending packet, default: 0.2")
     (options, args) = parser.parse_args()
 
     if not options.target or not '/' in options.target:
         print "please specify a target: -t 192.168.0.10/24"
         return
 
-    timeout = 2
-    count = 1
-
-    if options.timeout:
-        timeout = options.timeout
-
+    interval = (options.interval if options.interval else '0.2')
+    timeout = (options.timeout if options.timeout else '2')
+    count = (options.count if options.count else '1')
     target = options.target
     net = Network(target)
 
     hosts_up = []
     for ip in Network(str(net.host_first()) + '/' + target.split('/')[1]):
-        if subprocess.call(['ping', '-i', '0.2', '-w', str(timeout), '-c', str(count), str(ip)]) == 0:
+        if subprocess.call(['ping', '-i', interval, '-w', timeout, '-c', count, str(ip)]) == 0:
             hosts_up.append(ip)
 
     print "Hosts up:"
